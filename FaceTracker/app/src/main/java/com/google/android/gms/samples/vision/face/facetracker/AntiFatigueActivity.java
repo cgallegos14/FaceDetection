a128c0e769e7a48e5c92d5dc4dbf34454b29fc75
Cristobal Gallegos <crisgallegos14@gmail.com>
2016-04-26 19:59:39 -0600

    Can now make call when alert is triggered

--- FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/AntiFatigueActivity.java
+++ FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/AntiFatigueActivity.java
@@ -1,26 +1,97 @@
 package com.google.android.gms.samples.vision.face.facetracker;
 
+import android.Manifest;
 import android.content.Intent;
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class AntiFatigueActivity extends Activity {
+public class AntiFatigueActivity extends ListActivity {
+
+    @Override
+    public long getSelectedItemId() {
+        return super.getSelectedItemId();
+
+    }
+
+    @Override
+    public int getSelectedItemPosition() {
+        return super.getSelectedItemPosition();
+    }
+
+    ListView lv;
+    Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anti_fatigue);
 
+        //TextView tv=(TextView)findViewById(R.id.tv);
+
+
         Button stopAlertButton = (Button) findViewById(R.id.stopAlert);
         stopAlertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in=new Intent(AntiFatigueActivity.this,FaceTrackerActivity.class);
+                Intent in = new Intent(AntiFatigueActivity.this, FaceTrackerActivity.class);
                 startActivity(in);
                 finish();
+            }
+        });
+
+
+        try {
+            c = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            startManagingCursor(c);
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+            //tv.setText(e.toString());
+        }
+
+        final String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+
+        SimpleCursorAdapter sadapt = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, c, from, to);
+        setListAdapter(sadapt);
+
+        lv = getListView();
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(getBaseContext(),from[position].toString(),Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                Toast.makeText(getBaseContext(),String.valueOf(position),Toast.LENGTH_LONG).show();
+                intent.setData(Uri.parse("tel:" + "5757700010"));
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
             }
         });
     }
